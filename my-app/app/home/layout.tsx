328827603b034c178ddaddabbf142acfa2532244
zhaiqiuyang <zhaiqiuyang@shimo.im>
2025-08-14 18:09:14 +0800

    fix: home状态问题

--- my-app/app/home/layout.tsx
+++ my-app/app/home/layout.tsx
@@ -1,21 +1,15 @@
 'use client';
 
 import React, { createContext, useContext, useState } from 'react';
-import { Navigation } from '@/components/navigation';
 import { Qoan } from '@/components/base';
-import { navigationItems } from '@/src/info';
 
 const TabContext = createContext<string>('');
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    const [activeTab, setActiveTab] = useState('00170001');
 
     return (
         <Qoan>
-            <TabContext.Provider value={activeTab}>
-                <Navigation items={navigationItems} selected={activeTab} onSelected={setActiveTab} />
-                <main>{children}</main>
-            </TabContext.Provider>
+            <main>{children}</main>
         </Qoan>
     );
 }
--- my-app/app/home/page.tsx
+++ my-app/app/home/page.tsx
@@ -1,21 +1,34 @@
 'use client';
 
 import { Qoan } from "@/components/base";
-import { useActiveTab } from "./layout";
-export default function Home() {
-  const activeTab = useActiveTab();
-  return (
-    <Qoan>
-      {activeTab === '00170001' && <HomePage></HomePage>}
+import { Navigation } from "@/components/navigation";
+import { navigationItems } from "@/src/info";
+import { useState } from "react";
 
+export default function Home() {
+  const [activeTab, setActiveTab] = useState('00170001');
+
+  return (
+
+    <Qoan>
+      <Navigation items={navigationItems} selected={activeTab} onSelected={setActiveTab} />
+      {activeTab === '00170001' && <HomePage />}
+      {activeTab === '00170002' && <OtherPage />}
     </Qoan>
   );
 }
 
-const HomePage = ()=>{
-  return(
+const HomePage = () => {
+  return (
     <Qoan>
       这是首页
     </Qoan>
-  )
-}
+  );
+};
+const OtherPage = () => {
+  return (
+    <Qoan>
+      这是其他
+    </Qoan>
+  );
+};
